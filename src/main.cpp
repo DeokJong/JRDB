a92280e7224053202ce1af9719996daeb0e69944
DeokJong <novelss444@jbnu.ac.kr>
2025-02-26 00:38:58 +0900

    chore: 간단한 main 구성

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "storage/StorageEngine.h"
 
-int main(int argc, char const *argv[])
-{
-  std::cout<<"hello world!";
-  return 0;
+int main() {
+    StorageEngine se("./temp");
+
+    se.createTable("users", {"id", "name", "age"}, {"INT", "VARCHAR", "INT"});
+
+    se.insertRecord("users", {"1", "Alice", "24"});
+    se.insertRecord("users", {"2", "Bob", "30"});
+
+    std::cout << "=== table users data ===" << std::endl;
+    se.selectRecords("users");
+    se.insertRecord("users", { "3", "Jin", "99" });
+    se.selectRecords("users");
+    return 0;
 }
